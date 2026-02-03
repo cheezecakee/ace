0f7e3f2aeaa457ad799ee9dbaf2bea0b8f429dbf
cheezecakee <anabel.pmoraes@gmail.com>
2026-02-03 11:24:45 -0300

    feat: add initilization

--- cmd/quiz/main.go
+++ cmd/quiz/main.go
@@ -1,6 +1,22 @@
-package main 
+package main
 
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+
+	"github.com/cheezecakee/ace/internal/ui/context"
+	screen "github.com/cheezecakee/ace/internal/ui/screens"
+)
 
 func main() {
-}
+	ctx := context.NewContext()
+	model := screen.NewModel(ctx)
 
+	p := tea.NewProgram(model)
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+}
